56ad8e92925bbf8c24b9d095bd6250227420d2de
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-05-10 18:36:01 +0200

    Simple save to image (as control) working.

--- main.js
+++ main.js
@@ -4,34 +4,62 @@
 
 var leafletImage = require('leaflet-image');
 
-L.Control.Export = L.Control.extend({
-    options: {
-        position: 'topleft'
-    },
+var imageDiv;
 
-    onAdd: function (map) {
-        this._map = map;
-        var container = L.DomUtil.create('input');
-        container.type="button";
-        container.value = "Save...";
-        container.onclick = function() {
+function createButton(pos, name, dimX, dimY) {
+    return {
+        options: {
+            position: pos
+        },
+
+        onAdd: function (map) {
+            this._map = map;
+            var container = L.DomUtil.create('input');
+            container.type = "button";
+            container.value = name;
+            container.onclick = function() {
+                leafletImage(mymap, function(err, canvas) {
+                    // now you have canvas
+                    var img = document.createElement('img');
+                    var dimensions = mymap.getSize();
+                    img.width = dimensions.x / 10;
+                    img.height = dimensions.y / 10;
+                    img.className = "image_output";
+                    img.src = canvas.toDataURL();
+                    imageDiv.innerHTML = '';
+                    imageDiv.appendChild(img);
+                });
+            };
+            return container;
+        }
+    }
+}
+
+function createOutput(pos) {
+    return {
+        options: {
+            position: pos
+        },
+
+        onAdd: function (map) {
+            this._map = map;
+            var container = L.DomUtil.create('div');
+            container.id = "images";
+            imageDiv = container;
+            return container;
+        }
+
+    }
+}
 
 
-            leafletImage(mymap, function(err, canvas) {
-                // now you have canvas
-                var img = document.createElement('img');
-                var dimensions = mymap.getSize();
-                img.width = dimensions.x;
-                img.height = dimensions.y;
-                img.src = canvas.toDataURL();
-                document.getElementById('images').innerHTML = '';
-                document.getElementById('images').appendChild(img);
-            });
-        };
-        return container;
-    },
+var a4width = 1654;
+var a4height = 2339;
 
-});
+L.Control.Save = L.Control.extend(createButton("bottomleft", "Save..."));
+L.Control.SaveLandscape = L.Control.extend(createButton("bottomleft", "A4 Landscape", a4width, a4height ));
+L.Control.SavePortrait = L.Control.extend(createButton("bottomleft", "A4 Portrait", a4height, a4width));
+L.Control.Output = L.Control.extend(createOutput("bottomleft"));
 
 L.Map.mergeOptions({
     exportControl: true
@@ -39,13 +67,12 @@ L.Map.mergeOptions({
 
 L.Map.addInitHook(function () {
     if (this.options.exportControl) {
-        this.exportControl = new L.Control.Export();
+        this.exportControl = new L.Control.Save();
+        this.exportControlLandscape = new L.Control.SaveLandscape();
+        this.exportControlPortrait = new L.Control.SavePortrait();
+        this.addControl(this.exportControlLandscape);
+        this.addControl(this.exportControlPortrait);
         this.addControl(this.exportControl);
+        this.addControl(new L.Control.Output());
     }
 });
-
-L.control.exportControl = function (options) {
-    return new L.Control.Export(options);
-};
-
-
