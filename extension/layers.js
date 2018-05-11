52fb51c99453834d3f9c8a5aa19c8c1650776f43
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-05-11 18:52:21 +0200

    NG tile layer selection working (save not working yet).

--- extension/layers.js
+++ extension/layers.js
@@ -20,6 +20,12 @@ function generateMapLayers(token){
 	var openTopoAttr = osmAttr + ', Tiles courtesy of <a href="https://opentopomap.org" target="_blank">OpenTopoMap.org</a>';
     var mapboxAttr = osmAttr + ', Tiles courtesy of <a href="https://www.mapbox.com" target="_blank">Mapbox.com</a>';
 	return [
+        {type: "tracksgl", name: "Tracks&Routes (GL)",
+            // NG style - cjh226q3j0rtd2roxwnwlwy13
+            style: "mapbox://styles/ospanel/cjkbfwccz11972rmt4xvmvme6",
+            opts: {maxZoom: 20, maxNativeZoom: 19, attribution: mapboxAttr},
+            grid: true,
+        },
         {type: "routes-ex", name: "Tracks&Routes",
             url: "https://api.mapbox.com/styles/v1/ospanel/cjkbfwccz11972rmt4xvmvme6/tiles/256/{z}/{x}/{y}?access_token=" + token,
             opts: {maxZoom: 20, maxNativeZoom: 19, attribution: mapboxAttr},
