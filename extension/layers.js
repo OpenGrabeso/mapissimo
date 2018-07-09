cd23833298255ebacb3f338964c091079c148294
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-07-09 02:04:17 +0200

    Strava heatmap experiments (no-auth zoom).

--- extension/layers.js
+++ extension/layers.js
@@ -14,6 +14,7 @@ function generateMapLayers(token){
 	var osmAttr = '&copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 	var mtbMapAttr = osmAttr + ', Tiles courtesy of <a href="http://mtbmap.cz/" target="_blank">mtbmap.cz</a>';
 	var osmCzAttr = ', Tiles courtesy of <a href="https://openstreetmap.cz" target="_blank">openstreetmap.cz</a>';
+    var stravaAttr = ', Heatmap courtesy of <a href="https://www.strava.com" target="_blank">Strava</a>';
 	var mapyCzOnlyAttr = '&copy; <a href="https://www.seznam.cz/" target="_blank">Seznam.cz, a.s</a>';
 	var mapyCzAttr = mapyCzOnlyAttr + ', ' + osmAttr;
 	var openTopoAttr = osmAttr + ', Tiles courtesy of <a href="https://opentopomap.org" target="_blank">OpenTopoMap.org</a>';
@@ -23,6 +24,14 @@ function generateMapLayers(token){
             url: "https://api.mapbox.com/styles/v1/ospanel/cjhbykqgr07cz2rph29u6h6yl/tiles/256/{z}/{x}/{y}?access_token=" + token,
             opts: {maxZoom: 20, maxNativeZoom: 19, attribution: mapboxAttr},
             grid: true,
+        },
+        {type: "routesheat", name: "Tracks&Routes+Heatmap",
+            url: "https://api.mapbox.com/styles/v1/ospanel/cjhbykqgr07cz2rph29u6h6yl/tiles/256/{z}/{x}/{y}?access_token=" + token,
+            opts: {maxZoom: 20, maxNativeZoom: 19, attribution: mapboxAttr},
+            grid: true,
+            overlay:
+                {url: "https://heatmap-external-{s}.strava.com/tiles/run/bluered/{z}/{x}/{y}.png?px=256",
+                    opts: {minZoom: 2, maxZoom: 20, maxNativeZoom: 12, subdomains: "abc", attribution: stravaAttr}}
         },
 		{type: "openstreetmap", name: "OpenStreetMap",
 			url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
