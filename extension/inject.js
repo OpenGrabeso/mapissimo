8ab99b0ae9c83f70f1143b3a2042a45977e5d23f
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-06-03 21:08:10 +0200

    Mapbox GL with leaflet working.

--- extension/inject.js
+++ extension/inject.js
@@ -36,27 +36,37 @@
 
             s.onload = function() {
 
-                var s = document.createElement("script");
-                s.src = getURL('leaflet-mapbox-gl.js');
-                s.type = 'text/javascript';
-                console.log("inject leaflet-mapbox-gl.js");
+                var s = document.createElement("link");
+                s.href = getURL('mapbox-gl.css');
+                s.rel = "stylesheet";
+                s.type = 'text/css';
+                console.log("inject mapbox-gl.css");
                 document.body.appendChild(s);
 
                 s.onload = function() {
 
                     var s = document.createElement("script");
-                    s.src = getURL('leaflet.grid.js');
+                    s.src = getURL('leaflet-mapbox-gl.js');
                     s.type = 'text/javascript';
-                    console.log("inject leaflet.grid.js");
+                    console.log("inject leaflet-mapbox-gl.js");
                     document.body.appendChild(s);
 
                     s.onload = function () {
+
                         var s = document.createElement("script");
-                        s.src = getURL('fix.js');
+                        s.src = getURL('leaflet.grid.js');
                         s.type = 'text/javascript';
-                        s.dataset.layersUrl = getURL('layers.js');
-                        console.log("inject layers.js");
+                        console.log("inject leaflet.grid.js");
                         document.body.appendChild(s);
+
+                        s.onload = function () {
+                            var s = document.createElement("script");
+                            s.src = getURL('fix.js');
+                            s.type = 'text/javascript';
+                            s.dataset.layersUrl = getURL('layers.js');
+                            console.log("inject layers.js");
+                            document.body.appendChild(s);
+                        }
                     }
                 }
             };
